9244cf4eccde5b6a01ec66ca0c86e5c2864ba7c9
David Agudelo <agudelo.david@hotmail.com>
2025-04-10 16:56:03 -0400

    post terminal py setup.py bdist_wheel sdist

--- build/lib/TercerRepo/__init__.py
+++ build/lib/TercerRepo/__init__.py
@@ -1,5 +1,4 @@
 def saludar(nombre):
     return "Hola " + nombre + ", este es mi primer paquete pip!" #metodo de Echo
 #en la terminal
-# pip install setuptools y elegir el entorno de python
 #> python3 setup.py bdist_wheel sdist "estas son las instrucciones para crear el paquete que necesito"
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="TercerRepo",                           # Nombre del paquete
+    name="TercerRepo",                              # Nombre del paquete
     version="0.1.0",                                # Versión inicial
     packages=find_packages(),                       # Paquetes a incluir
     description="Un paquete pip simple de saludo",  # Breve descripción
-    author="agudelo.david",                         # nombre
+    author="agudelo David",                         # nombre
     author_email="agudelo.david@hotmail.com",       # correo electrónico
     url="https://github.com/dagudelob/TercerRepo",  # URL del proyecto
-)
+    )
